0cf4870c9b75fa761c36832b21d6ac497e7d7458
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-05 03:27:57 +0300

    Added function to allow users update their profile and bio

--- instasocial/views.py
+++ instasocial/views.py
@@ -36,7 +36,7 @@ def signup(request):
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
