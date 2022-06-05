876dc4c675606ca52bed909ddada4cfc2b91f378
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-05 13:21:47 +0300

    Created the Post model allowing users to create posts

--- instasocial/models.py
+++ instasocial/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -16,3 +18,13 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
