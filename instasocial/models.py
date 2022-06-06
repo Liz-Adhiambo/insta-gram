9e34a7d988fab051254e30a74ef8eee3c1e0bb88
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-06 17:13:46 +0300

    Created the comment model to enable users add comments

--- instasocial/models.py
+++ instasocial/models.py
@@ -42,3 +42,12 @@ class FollowersCount(models.Model):
 
     def __str__(self):
         return self.user
+
+class Comment(models.Model):
+    postc=models.ForeignKey(Post,related_name='comments',on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    body=models.TextField()
+    date_added=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s-%s' % (self.post.id, self.name)
